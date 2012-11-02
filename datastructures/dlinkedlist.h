67cce5a02ab66407b7d4fa65770c4868de8a0fa2
Andrei Vacariu <vacariu.v@gmail.com>
2012-11-01 17:48:16 -0700

    Added a licnese preamble to all the files because the license says '<Name> write this file.'

--- datastructures/dlinkedlist.h
+++ datastructures/dlinkedlist.h
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE":
+ * <Andrei Vacariu> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ * ----------------------------------------------------------------------------
+ */
+
 #include "errors.h"
 
 struct node {
